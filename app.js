77e3ae9761f9d83e298cace6cde3d7e5cc75cc09
jzhupan <jzhupan@gmail.com>
2023-01-18 19:37:22 -0500

    changed the button function for now.

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-function convertButton() {
-    document.getElementsByClassName("btn btn-primary").onclick = function() {convertButton()};
-    console.log('Tried to convert');
+function clickedButton() {
+    console.log('Converted');
 }
