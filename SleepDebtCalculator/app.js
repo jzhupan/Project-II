59a1e31972514e91f5ebac8103a3b4153f70a0b8
jzhupan <jzhupan@gmail.com>
2023-01-26 11:10:48 -0500

    Was able to solve what I left off yesterday.

--- SleepDebtCalculator/app.js
+++ SleepDebtCalculator/app.js
@@ -1,27 +1,27 @@
 const getSleepHours = day => {
-    switch (day) {
-      case 'Monday':
-      return 7;
-      break;
-      case 'Tuesday':
-      return 8;
-      break;
-      case 'Wednesday':
-      return 8;
-      break;
-      case 'Thursday':
-      return 8;
-      break;
-      case 'Friday':
-      return 5;
-      break;
-      case 'Saturday':
-      return 9;
-      break;
-      case 'Sunday':
-      return 9;
-      break;
-  }
+  switch (day) {
+    case 'Monday':
+    return 7;
+    break;
+    case 'Tuesday':
+    return 8;
+    break;
+    case 'Wednesday':
+    return 8;
+    break;
+    case 'Thursday':
+    return 8;
+    break;
+    case 'Friday':
+    return 5;
+    break;
+    case 'Saturday':
+    return 9;
+    break;
+    case 'Sunday':
+    return 9;
+    break;
+}
 };
 // console.log(getSleepHours('Monday'));
 
@@ -31,25 +31,25 @@ let getActualSleepHours = () =>  7 + 8 + 8 + 8 + 5 + 9 + 9;
 // console.log(getActualSleepHours());
 
 const getIdealSleepHours = idealHours => {
-  return idealHours * 8;
+return idealHours * 8;
 }
 
 // console.log(getIdealSleepHours(8));
 
 const calculateSleepDebt = () => {
-  actualSleepHours = getActualSleepHours(56);
-  idealSleepHours = getIdealSleepHours(8);
-  if (actualSleepHours === idealSleepHours) {
-    return 'You got the perfect amount of sleep!';
-  } else if (actualSleepHours > idealSleepHours){
-    return 'Sleeping too much! You are sleeping ' + (Math.abs(idealSleepHours - actualSleepHours)) + ' more hours than you need!';
-  } else if (actualSleepHours < idealSleepHours) {
-  return 'You need some rest. You are sleeping ' + (idealSleepHours - actualSleepHours) + ' hours less.';
-  } else {
-    return 'Are you a Vampire?';
-  }
+actualSleepHours= 65;
+idealSleepHours = getIdealSleepHours(8);
+if (actualSleepHours === idealSleepHours) {
+  return 'You got the perfect amount of sleep!';
+} else if (actualSleepHours > idealSleepHours){
+  return 'Sleeping too much! You are sleeping ' + (Math.abs(idealSleepHours - actualSleepHours)) + ' more hour(s) than you need!';
+} else if (actualSleepHours < idealSleepHours) {
+return 'You need some rest. You are sleeping ' + (idealSleepHours - actualSleepHours) + ' hour(s) less.';
+} else {
+  return 'Are you a Vampire?';
+}
 }
 console.log(calculateSleepDebt());
 
+// calculateSleepDebt();
 
-//Coming back to this tomorrow. Need to rewrite it in a way we can calculate Sleep debt with customized actual sleep hours and ideal sleep hours.
