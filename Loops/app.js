5a1170dc6adc3f24a50214d368148bcba2c63963
jzhupan <jzhupan@gmail.com>
2023-02-01 18:22:22 -0500

    For loops practice, did a Nested loop exercise.

---
+++ Loops/app.js
@@ -0,0 +1,12 @@
+const bobsFollowers = ["Amelia", "David", "Alex", "Victor"];
+const tinasFollowers = ["Amelia", "Alex", "Belle"];
+let mutualFollowers = [];
+
+for (let i = 0; i < bobsFollowers.length; i++) {
+  for (let j = 0; j < tinasFollowers.length; j++) {
+    if (bobsFollowers[i] === tinasFollowers[j]) {
+      mutualFollowers.push(bobsFollowers[i]);
+    }
+  }
+}
+console.log(mutualFollowers);
