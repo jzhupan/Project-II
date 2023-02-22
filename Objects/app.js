0a5b9c707017bd28466706798d8b60a7a6f932f1
jzhupan <jzhupan@gmail.com>
2023-02-21 20:53:49 -0500

    Exposed to Nested Objects today.

--- Objects/app.js
+++ Objects/app.js
@@ -43,3 +43,39 @@ let spaceship = {
   spaceship.color = 'glorious gold';
   spaceship.numEngines = Math.random()*10;
   delete spaceship['Secret Mission'];
+
+  //Nested objects
+  let spaceship = {
+    passengers: [{name: 'Amelia'}],
+    telescope: {
+      yearBuilt: 2018,
+      model: "91031-XLT",
+      focalLength: 2032 
+    },
+    crew: {
+      captain: { 
+        name: 'Sandra', 
+        degree: 'Computer Engineering', 
+        encourageTeam() { console.log('We got this!') },
+       'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+    },
+    engine: {
+      model: "Nimbus2000"
+    },
+    nanoelectronics: {
+      computer: {
+        terabytes: 100,
+        monitors: "HD"
+      },
+      'back-up': {
+        battery: "Lithium",
+        terabytes: 50
+      }
+    }
+  }; 
+  
+  let capFave = spaceship.crew.captain['favorite foods'][0];
+  console.log(capFave);
+  
+  let firstPassenger = spaceship.passengers[0]
+  console.log(firstPassenger)
