587177c2e53757249ba79e7131a3eae58ec17f63
jzhupan <jzhupan@gmail.com>
2023-02-24 17:00:29 -0500

    Gave it another read and I somewhat understand pass by references objects, was able to solve the small exercise today.

--- Objects/app.js
+++ Objects/app.js
@@ -90,3 +90,11 @@ let spaceship = {
   let greenEnergy = spaceship => {
      spaceship['Fuel Type'] = 'avocado oil';
   }
+  
+  let remotelyDisable= spaceship => {
+    spaceship.disabled = true
+  }
+  
+  greenEnergy(spaceship);
+  remotelyDisable(spaceship);
+  console.log(spaceship);
