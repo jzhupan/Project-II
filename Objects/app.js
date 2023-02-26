0e243680fdfb7a73fc9192700eda1cce070b794e
jzhupan <jzhupan@gmail.com>
2023-02-25 19:52:16 -0500

    WIP Looping through objects exercise.

--- Objects/app.js
+++ Objects/app.js
@@ -98,3 +98,39 @@ let spaceship = {
   greenEnergy(spaceship);
   remotelyDisable(spaceship);
   console.log(spaceship);
+
+
+
+
+  
+  // Looping through objects
+  let spaceship = {
+    crew: {
+    captain: { 
+        name: 'Lily', 
+        degree: 'Computer Engineering', 
+        cheerTeam() { console.log('You got this!') } 
+        },
+    'chief officer': { 
+        name: 'Dan', 
+        degree: 'Aerospace Engineering', 
+        agree() { console.log('I agree, captain!') } 
+        },
+    medic: { 
+        name: 'Clementine', 
+        degree: 'Physics', 
+        announce() { console.log(`Jets on!`) } },
+    translator: {
+        name: 'Shauna', 
+        degree: 'Conservation Science', 
+        powerFuel() { console.log('The tank is full!') } 
+        }
+    }
+}; 
+
+// Write your code below
+for (let crewMember in spaceship.crew) {
+  console.log(
+    `[crew member's role]: [crew member's name].spaceship`
+  )
+}
